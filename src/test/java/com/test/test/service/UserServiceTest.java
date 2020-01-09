2c1f3383e9d21542a8e825f6652b0a95bf127ddf
Sujan Limbu <sujanlimbu8@gmail.com>
2020-01-09 09:48:01 +0545

    .

---
+++ src/test/java/com/test/test/service/UserServiceTest.java
@@ -0,0 +1,11 @@
+package com.test.test.service;
+
+import org.junit.Test;
+
+public class UserServiceTest {
+
+    @git
+    public void deleteUser_invalidId(){
+        UserService userService = new UserService();
+    }
+}
